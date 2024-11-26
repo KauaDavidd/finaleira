2b0533f8394965ed45ee6d877f11759c1a86e94e
KauaDavidd <kauadavidrosa@gmail.com>
2024-11-26 20:38:27 -0300

    oie

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,6 +13,33 @@ function Login() {
             const response = await axios.post('https//localhost:8000/login/', loginData);
             setToken(reponse.data.access_token);
             alert('Login realizado com sucesso!');
+        } catch (error) {
+            alert('Erro ao fazer login');
         }
-    }
+    };
+
+    return(
+        <div>
+        <h2>Login</h2>
+        <from onSubmit={handleSubmit}>
+        <input
+            type="text"
+            placeholder="Nome de Usuário"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            />
+        <input
+            type="password"
+            placeholder="Senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            required
+            />
+        <button type= "submit">Entrar</button>
+        </from>    
+        </div>
+    );
 }
+
+export default Login;
---
+++ src/Reserve.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+ 
+function Reserve() {
+  const [dates, setDates] = useState([]);
+  const [selectedDate, setSelectedDate] = useState('');
+  const [token, setToken] = useState(localStorage.getItem('token')); // Armazena o token JWT
+ 
+  useEffect(() => {
+    axios.get('http://localhost:8000/available_dates/')
+      .then((response) => {
+        setDates(response.data);
+      })
+      .catch((error) => {
+        alert('Erro ao carregar datas');
+      });
+  }, []);
+ 
+  const handleReserve = () => {
+    axios.post('http://localhost:8000/reserve/', { date: selectedDate }, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        alert('Reserva realizada com sucesso!');
+      })
+      .catch((error) => {
+        alert('Erro ao fazer reserva');
+      });
+  };
+ 
+  return (
+<div>
+<h2>Reservar Evento</h2>
+<select onChange={(e) => setSelectedDate(e.target.value)}>
+        {dates.map((date) => (
+<option key={date} value={date}>{date}</option>
+        ))}
+</select>
+<button onClick={handleReserve}>Reservar</button>
+</div>
+  );
+}
+ 
+export default Reserve;
